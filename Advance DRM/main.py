c5fe4bc8197dfa81466a0f85c0861e67dd9d943f
mangalshashank <mangalshashank1@gmail.com>
2024-03-28 19:16:14 +0530

    added linkedin oauth

--- Advance DRM/main.py
+++ Advance DRM/main.py
@@ -7,6 +7,7 @@ import getHash
 import userDataStorage
 from PIL import Image
 import os
+import requests
 
 app = Flask(__name__)
 DATABASE = 'user_database.db'
@@ -15,6 +16,60 @@ connect = sqlite3.connect(DATABASE,check_same_thread=False)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+# LinkedIn API credentials
+CLIENT_ID = '86pvg78zp7n1yl'
+CLIENT_SECRET = 'Zdw6LlhqHCBkg2zF'
+REDIRECT_URI = 'http://localhost:5000/callback'  # This should match with your LinkedIn app settings
+
+@app.route('/linkedInlogin', methods=['GET', 'POST'])
+def linkedInlogin():
+    params = {
+        'response_type': 'code',
+        'client_id': CLIENT_ID,
+        'redirect_uri': REDIRECT_URI,
+        'scope': 'profile email openid',  # Adjust scope based on your requirements
+    }
+    auth_url = 'https://www.linkedin.com/oauth/v2/authorization?' + '&'.join([f'{key}={value}' for key, value in params.items()])
+    return redirect(auth_url)
+
+@app.route('/callback', methods=['GET', 'POST'])
+def callback():
+    code = request.args.get('code')
+    if code:
+        token_url = 'https://www.linkedin.com/oauth/v2/accessToken'
+        data = {
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': REDIRECT_URI,
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
+        }
+        response = requests.post(token_url, data=data)
+        access_token = response.json().get('access_token')
+        if access_token:
+            headers = { 'Authorization': f'Bearer {access_token}' }
+            profile_url = 'https://api.linkedin.com/v2/userinfo'
+            response = requests.get(profile_url, headers=headers)
+            profile_data = response.json()
+            return render_template('linkedIn-details.html',user=profile_data)
+        else:
+            return "Failed to obtain access token"
+    else:
+        return "Error in callback"
+
+@app.route('/verify_email', methods=['POST'])
+def verify_email():
+    email = request.form['email']
+    print(email)
+    cursor = connect.cursor()
+    cursor.execute('SELECT * FROM validemail WHERE emailId = ?', (email,))
+    user = cursor.fetchone()
+    if user is not None:
+    # Redirect back to the index page or render a success page
+        return redirect(url_for('getMarksheet'))
+    else:
+        return render_template('invalid-address.html')
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
